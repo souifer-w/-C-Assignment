3d0fd751b0f1faea3443fa1bcae8851fb1532e8b
HP <HP@DESKTOP-NMBILJD>
2024-11-02 17:54:22 +0100

    Add project files.

---
+++ Assignment/Assignment.cpp
@@ -0,0 +1,26 @@
+#include < iostream>
+using namespace std;
+
+int main() {
+	int A = 10 , B = 20;
+	A += B;
+	cout << "A =  " << A << endl;
+	A -= B;
+	cout << "A =" << A << endl;
+	A *= B;
+	cout << "A = " << A << endl;
+    A /= A;
+	cout << " A = " << A << endl;
+	A %= B;
+	cout << "A = " << A << endl;
+
+
+
+
+
+
+
+
+
+	return 0;
+}
